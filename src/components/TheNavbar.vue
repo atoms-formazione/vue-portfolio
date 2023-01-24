32037b82acf4c763b847e31fff5d0adbb0c623a0
Daniele Ghilardini <ghilardinidaniele@gmail.com>
2023-01-24 15:34:30 +0100

    Fixed Navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,22 +1,31 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
+  <div class="all">
     <h1 class="name">John Doe</h1>
-    <h1 class="navbar">home about work</h1>
-    <img src="./icons/vector.svg" alt="vector" class="vector" />
-    <img src="./icons/behance.svg" alt="behance" class="behance" />
-    <img src="./icons/twitter.svg" alt="twitter" class="twitter" />
+    <div class="centered">
+      <h1 class="navbar">home</h1>
+      <h1 class="navbar">about</h1>
+      <h1 class="navbar">work</h1>
+    </div>
+    <div class="icons">
+      <img src="./icons/vector.svg" alt="vector" class="vector" />
+      <img src="./icons/behance.svg" alt="behance" class="behance" />
+      <img src="./icons/twitter.svg" alt="twitter" class="twitter" />
+    </div>
   </div>
 </template>
 
 <style scoped>
+.all {
+  display: flex;
+  flex-direction: row;
+  align-content:center;
+}
 .name {
-  position: absolute;
+  flex: 1;
   width: 114px;
   height: 36px;
-  left: 97px;
-  top: 34px;
 
   font-family: "Poppins";
   font-style: normal;
@@ -26,36 +35,38 @@
 
   color: #03045e;
 }
+.centered {
+  flex: 1;
+  display: flex;
+}
+
+.icons {
+  flex: 0.5;
+  display: flex;
+  align-items: right;
+}
 
 .navbar {
-  position: absolute;
+  flex: 1;
   width: 276px;
   height: 27px;
-  left: 582px;
-  top: 37px;
 }
 
 .vector {
-  position: absolute;
+  flex: 1;
   width: 26px;
   height: 15.05px;
-  left: 1201px;
-  top: 42px;
 }
 
 .behance {
-  position: absolute;
+  flex: 1;
   width: 26px;
   height: 16.1px;
-  left: 1256px;
-  top: 42px;
 }
 
 .twitter {
-  position: absolute;
+  flex: 1;
   width: 26px;
   height: 17.79px;
-  left: 1311px;
-  top: 41px;
 }
 </style>
