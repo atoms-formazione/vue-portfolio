465ddf45e7590840395dbdf797da990ee16851f8
Alessandro Tecchio <tecchio.alessandro.96@gmail.com>
2023-01-24 17:44:37 +0100

    feat: header and hero

---
+++ src/components/TheHeadBarLeft.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Alessandro Tecchio</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+div {
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 1000;
+  font-size: 24px;
+  height: fit-content;
+  color: #03045e;
+}
+</style>
---
+++ src/components/WelcomeIntro.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="mainIntro">
+    <div class="container">
+      <p>Hello I'm a monke</p>
+      <strong>Uh-oh stinky!</strong>
+      <p>Based in Italy</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.mainIntro {
+  background-color: blue;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.container {
+  width: 100%;
+}
+
+.container strong {
+  font-size: 5rem;
+  font-weight: 800;
+  text-align: center;
+}
+
+.container p {
+  text-align: center;
+}
+</style>
