cb20a3fac2fced04fb383ec78f7964a87f9511bf
Pietro Stivanello <stivanellop@yahoo.com>
2023-01-24 16:53:52 +0100

    day1

---
+++ src/components/theHero.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="flex-container">
+        
+        <div class="intro">
+            <WelcomeIntro></WelcomeIntro>
+        </div>
+        <div class="avatar">
+            <BigAvatar></BigAvatar>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import BigAvatar from './bigAvatar.vue';
+import WelcomeIntro from './welcomeIntro.vue';
+
+
+</script>
+
+<style scoped>
+.flex-container{
+    display: flex;
+    margin-top: 5rem;
+    
+}
+.intro{
+    flex: 1;
+}
+.avatar{
+    flex: 1;
+    text-align: center;
+}
+</style>
---
+++ src/components/welcomeIntro.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <strong class="job"> {{ job }} </strong>
+  </div>
+</template>
+
+<script setup lang="ts">
+const job = "Software Developer";
+</script>
+
+<style scoped>
+.job {
+  font-size: 4rem;
+}
+.job::before {
+  content: "Hello, I'm Pietro";
+  font-size: 1rem;
+}
+.job::after {
+  content: "Based in Italy";
+  font-size: 1rem;
+}
+</style>
