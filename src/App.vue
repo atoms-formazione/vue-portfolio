d9ab8a9a70f46760ea66771553db551be3a16af2
Moneta02 <pietro.coin02@gmail.com>
2023-01-25 17:16:36 +0100

    add contact

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import AboutMe from "./components/AboutMe.vue";
 import NavBar from "./components/NavBar.vue";
 import TheHero from "./components/TheHero.vue";
 import WorkSpace from "./components/WorkSpace.vue";
+import Contatacs from "./components/Contatacs.vue";
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import WorkSpace from "./components/WorkSpace.vue";
       <div class="about"><AboutMe></AboutMe></div>
       <div class="workspace"><WorkSpace></WorkSpace></div>
     </div>
+    <Contatacs></Contatacs>
   </div>
 </template>
 
--- src/components/Contatacs.vue
+++ src/components/Contatacs.vue
@@ -1,6 +1,24 @@
-<template>
-    <div>
+<!-- eslint-disable vue/multi-word-component-names -->
 
+<template>
+    <div class="ContactSection">
+        <div class="title">
+            <h1>contatti.</h1>
+        </div>
+        <div class="description">
+            <div class="image">
+
+            </div>
+            <div class="links">
+                <p>
+
+                </p>
+                <br>
+                <p>
+
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,5 +27,22 @@
 </script>
 
 <style scoped>
+    div {
+        color: #14213d;
+    }
 
+    .ContactSection {
+        margin-left: 1rem;
+        margin-top: 2rem;
+        padding: 4rem;
+    }
+
+    .title{
+        margin: 2rem;
+    }
+     h1 {
+        font-size: 3rem;
+        font-weight: bolder;
+        color: #fca311;
+    }
 </style>
