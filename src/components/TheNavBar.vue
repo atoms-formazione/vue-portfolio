885fd2b77cd566c5a468a04ed1d0f8bef73a4e92
DoppiaB98 <lorenzobb_1990@libero.it>
2023-01-26 19:09:05 +0100

    #

---
+++ src/components/TheNavBar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="a">
+    <h1>Lorenzo B.B.</h1>
+    <h2>Home About Work</h2>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  position: absolute;
+  float: left;
+  font-family: "Courier New", Courier, monospace;
+}
+h2 {
+  position: absolute;
+  left: 50%;
+}
+#a {
+  border-width: 5px;
+  color: aqua;
+}
+</style>
