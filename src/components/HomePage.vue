9c6b1aaba7bd429b48795f9404ffac6a292d8bd6
Martin Masevski <martin.masevski@gmail.com>
2023-01-30 23:02:08 +0100

    feat: add about section

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import PresentationSection from "./sections/PresentationSection.vue";
+import AboutSection from "./sections/AboutSection.vue";
 </script>
 
 <template>
   <div class="sections">
     <PresentationSection />
+    <AboutSection />
   </div>
 </template>
 
---
+++ src/components/atoms/AboutList.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+type Experience = {
+  duration: string;
+  description: string;
+};
+
+type AboutListProps = {
+  list: Experience[];
+};
+
+defineProps<AboutListProps>();
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="{ duration, description } in list" :key="duration">
+        <h3>{{ duration }}</h3>
+        <p>{{ description }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+li {
+  margin-bottom: 80px;
+}
+
+h3 {
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 40px;
+}
+
+p {
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 44px;
+}
+</style>
