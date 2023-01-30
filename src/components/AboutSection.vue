5fb4f7732832abcf1649d0eda7939ba8cecc5fcc
DoppiaB98 <lorenzobb_1990@libero.it>
2023-01-30 23:15:26 +0100

    #

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
-//
+import TitleText1 from "./TitleText1.vue";
 </script>
 
 <template>
   <div>
-    <!-- -->
+    <TitleText1 id="titleText1Order"></TitleText1>
   </div>
 </template>
 
 <style scoped>
-/* */
+#titleText1Order {
+}
 </style>
---
+++ src/components/TitleText1.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <div>
+    <p id="bigAbout">About.</p>
+    <p id="titleText">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet vulputate
+      tristigue quam felis. phasellus dui orci vulputate consequat nulla proin.
+      Id sit scelerisque neque, proin bibendum diam.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+#bigAbout {
+  font-family: fantasy;
+  font-size: 50px;
+  color: rgb(196, 196, 134);
+  float: left;
+  position: relative;
+  top: 400px;
+  left: 30px;
+}
+#titleText {
+  font-family: "Courier New", Courier, monospace;
+  color: black;
+  float: left;
+  position: relative;
+  top: 345px;
+  left: 30px;
+  padding-right: 500px;
+}
+</style>
