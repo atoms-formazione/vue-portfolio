8b5fc14ff3b9fa98bf96dac5ea2d28a96c7ce90c
DoppiaB98 <lorenzobb_1990@libero.it>
2023-01-30 15:32:05 +0100

    #

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -2,40 +2,51 @@
 
 <template>
   <div class="topnav">
-    <b>Lorenzo B.B.</b>
-    <a href="url_home">Home</a>
-    <a href="url_about">About</a>
-    <a href="url_work">Work</a>
+    <a>Lorenzo B.B.</a>
+    <b href="url_home">Home</b>
+    <b href="url_about">About</b>
+    <b href="url_work">Work</b>
   </div>
 </template>
 
 <style scoped>
 .topnav {
   background-color: rgb(208, 208, 147);
+  font-family: "Courier New", Courier, monospace;
   overflow: hidden;
   width: 100%;
-}
-
-.topnav b {
-  float: left;
-  color: black;
-  text-align: left;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-  margin: 20px;
+  display: flex;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  font-weight: bold;
 }
 
 .topnav a {
   float: left;
   color: black;
+  text-align: center;
   padding: 14px 16px;
   text-decoration: none;
   font-size: 17px;
-  text-align: right;
+  margin: 20px;
+  position: fixed;
+  top: -12px;
+  left: 0px;
 }
 
-.topnav a:hover {
-  background-color: rgb(207, 207, 198);
+.topnav b {
+  float: left;
+  color: black;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  text-align: center;
+  position: relative;
+  font-weight: normal;
+}
+
+.topnav b:hover {
+  background-color: rgb(199, 199, 169);
 }
 </style>
