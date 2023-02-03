339d216494744e8e33e7d3f06bb2da57853dd43f
Kejmamr <kejsibitri01@gmail.com>
2023-02-03 18:33:59 +0100

    work

---
+++ src/components/WorkCard.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+type WorkCardProps = {
+  image: string;
+  date: string;
+  title: string;
+  description: string;
+};
+defineProps<WorkCardProps>();
+</script>
+
+<template>
+  <div class="card-container">
+    <img :src="`/src/components/icons/${image}.png`">
+    <h6>{{ date }}</h6>
+    <h4>{{ title }}</h4>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<style scoped>
+img {
+  object-fit: cover;
+  width: 100%;
+}
+h6 {
+  font-style: italic;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 44px;
+  color: #03045E;
+
+}
+h4 {
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 44px;
+  color: #03045E;
+
+}
+p {
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 32px;
+  color: #03045E;
+
+}
+</style>
---
+++ src/components/WorkCase.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import WorkCard from './WorkCard.vue';
+import TitleTextWork from './TitleTextWork.vue';
+const workCards = [
+  {
+    image: "Caso",
+    date: "November 24, 2019",
+    title: "Some case study",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed aliquam sollicitudin rhoncus morbi. Tincidunt quam sem elit a convallis. Eget ipsum, velit vitae eu nunc, consequat, at.",
+  },
+  {
+    image: "caso02",
+    date: "December 12, 2020",
+    title: "Other case study",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed aliquam sollicitudin rhoncus morbi. Tincidunt quam sem elit a convallis. Eget ipsum, velit vitae eu nunc, consequat, at.",
+  },
+];
+</script>
+
+<template>
+  <div class="work-container">
+    <TitleTextWork />
+
+    <div class="cards-wrapper">
+      <WorkCard v-for="card in workCards" :key="card.date" v-bind="card" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.work-container {
+  margin-top: 200px;
+}
+.title-section {
+  margin-bottom: 145px;
+}
+.cards-wrapper {
+  display: flex;
+  gap: 120px;
+}
+</style>
